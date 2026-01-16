9b159c4c1136be258abc29efd4cc24734c34baec
KiesheaH <59402191+KiesheaH@users.noreply.github.com>
2026-01-16 13:36:53 -0500

    Change application color scheme
    Update application colors to more subtle tones

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const calculator = document.querySelector(".container");
+const display = document.querySelector(".display");
+const calculation = document.querySelector(".calculation");
+const answer = document.querySelector(".answer");
+const button = document.querySelector(".btn");
+
+calculator.addEventListener("click", function () {
+  calculator.style.backgroundColor = "red";
+});
